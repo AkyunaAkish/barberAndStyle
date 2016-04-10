8cf2cdf8425bbb2d4f8bbc99d13620dc31b346ff
AkyunaAkish <akyunaakish@gmail.com>
2016-04-10 17:47:21 -0600

    testing nodemailer

--- public/javascripts/controllers/mainController.js
+++ public/javascripts/controllers/mainController.js
@@ -4,4 +4,17 @@ app.controller('MainController', function($scope, $http){
     $scope.message = res.data;
   });
 
+  $scope.sendEmail = function() {
+    var messageObj = {};
+    messageObj.senderEmail = $scope.emailAddress;
+    messageObj.senderName = $scope.senderName;
+    messageObj.emailSubject = $scope.emailSubject;
+    messageObj.emailMessage = $scope.emailMessage;
+
+    $http.post('/api/v1/send_email', messageObj).then(function(res){
+      alert(res.data);
+    });
+
+  };
+
 });
--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,39 @@
+require('dotenv').load();
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
 
-router.get('/test', function(req,res,next){
-  res.json('test');
+
+router.post('/send_email', function(req,res,next){
+  console.log('req.body ', req.body);
+  // create reusable transporter object using the default SMTP transport
+  var transporter = nodemailer.createTransport({
+    service: 'Aol',
+    auth: {
+      user: process.env.EMAIL, // Your email id
+      pass: process.env.PASS // Your password
+    }
+  });
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: req.body.senderEmail, // sender address
+    to: 'akyunaakish@gmail.com', // list of receivers
+    subject: "From: " + req.body.senderName + ": \n" + req.body.emailSubject, // Subject line
+    text: req.body.emailMessage, // plaintext body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log('err ', error);
+      res.json('Server error: ', error);
+    } else {
+      console.log('good res ', info.response);
+      res.json('Message sent: ' + info.response);
+    }
+  });
+  
 });
 
 module.exports = router;
