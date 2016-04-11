75f240e91f1335b7335461e4f92d8e3cf167737d
AkyunaAkish <akyunaakish@gmail.com>
2016-04-10 18:10:15 -0600

    testing nodemailer on heroku

--- routes/api.js
+++ routes/api.js
@@ -4,42 +4,69 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 
 router.post('/sendEmail', function(req,res,next){
-  res.status(200).json(req.body);
-});
-
-
-router.post('/send_email', function(req,res,next){
-
-  res.status(200).json(req.body)
-
   // create reusable transporter object using the default SMTP transport
-  // var transporter = nodemailer.createTransport({
-  //   service: 'AOL',
-  //   auth: {
-  //     user: process.env.EMAIL, // Your email id
-  //     pass: process.env.PASS // Your password
-  //   }
-  // });
-  //
-  // // setup e-mail data with unicode symbols
-  // var mailOptions = {
-  //   from: req.body.senderEmail, // sender address
-  //   to: process.env.EMAIL, // list of receivers
-  //   subject: "From: " + req.body.senderName + ": \n" + req.body.emailSubject, // Subject line
-  //   text: req.body.emailMessage, // plaintext body
-  // };
-  //
-  // // send mail with defined transport object
-  // transporter.sendMail(mailOptions, function(error, info){
-  //   if(error){
-  //     console.log('err ', error);
-  //     res.json('Server error: ', error);
-  //   } else {
-  //     console.log('good res ', info.response);
-  //     res.json('Message sent: ' + info.response);
-  //   }
-  // });
+  var transporter = nodemailer.createTransport({
+    service: 'AOL',
+    auth: {
+      user: process.env.EMAIL, // Your email id
+      pass: process.env.PASS // Your password
+    }
+  });
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+    from: req.body.senderEmail, // sender address
+    to: process.env.EMAIL, // list of receivers
+    subject: "From: " + req.body.senderName + ": \n" + req.body.emailSubject, // Subject line
+    text: req.body.emailMessage, // plaintext body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      console.log('err ', error);
+      res.status(503).json(error);
+    } else {
+      console.log('good res ', info.response);
+      res.status(200).json(info.response);
+    }
+  });
 
 });
+
+
+// router.post('/send_email', function(req,res,next){
+//
+//   res.status(200).json(req.body)
+//
+//   // create reusable transporter object using the default SMTP transport
+//   var transporter = nodemailer.createTransport({
+//     service: 'AOL',
+//     auth: {
+//       user: process.env.EMAIL, // Your email id
+//       pass: process.env.PASS // Your password
+//     }
+//   });
+//
+//   // setup e-mail data with unicode symbols
+//   var mailOptions = {
+//     from: req.body.senderEmail, // sender address
+//     to: process.env.EMAIL, // list of receivers
+//     subject: "From: " + req.body.senderName + ": \n" + req.body.emailSubject, // Subject line
+//     text: req.body.emailMessage, // plaintext body
+//   };
+//
+//   // send mail with defined transport object
+//   transporter.sendMail(mailOptions, function(error, info){
+//     if(error){
+//       console.log('err ', error);
+//       res.json('Server error: ', error);
+//     } else {
+//       console.log('good res ', info.response);
+//       res.json('Message sent: ' + info.response);
+//     }
+//   });
+//
+// });
 
 module.exports = router;
