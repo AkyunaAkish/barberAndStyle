de7510cf4129298426e79132dc017faed2a12c49
AkyunaAkish <akyunaakish@gmail.com>
2016-04-10 18:44:18 -0600

    implemented sendgrid

--- routes/api.js
+++ routes/api.js
@@ -1,72 +1,29 @@
 require('dotenv').load();
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
+var sendgrid  = require('sendgrid')(process.env.API_KEY);
+
 
 router.post('/sendEmail', function(req,res,next){
-  // create reusable transporter object using the default SMTP transport
-  var transporter = nodemailer.createTransport({
-    service: 'AOL',
-    auth: {
-      user: process.env.EMAIL, // Your email id
-      pass: process.env.PASS // Your password
-    }
-  });
 
-  // setup e-mail data with unicode symbols
-  var mailOptions = {
-    from: req.body.senderEmail, // sender address
-    to: process.env.EMAIL, // list of receivers
-    subject: "From: " + req.body.senderName + ": \n" + req.body.emailSubject, // Subject line
-    text: req.body.emailMessage, // plaintext body
-  };
+  var payload   = {
+    to      : process.env.EMAIL,
+    from    : req.body.senderEmail,
+    subject : req.body.emailSubject,
+    text    : "From: " + req.body.senderName +": \n"+ req.body.emailMessage
+  }
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      console.log('err ', error);
-      res.status(503).json(error);
+  sendgrid.send(payload, function(err, json) {
+    if (err) {
+      console.error(err);
+      res.status(503).json(err);
     } else {
-      console.log('good res ', info.response);
-      res.status(200).json(info.response);
-    }
+      console.log(json);
+      res.status(200).json(json);
+    }  
   });
 
 });
 
-
-// router.post('/send_email', function(req,res,next){
-//
-//   res.status(200).json(req.body)
-//
-//   // create reusable transporter object using the default SMTP transport
-//   var transporter = nodemailer.createTransport({
-//     service: 'AOL',
-//     auth: {
-//       user: process.env.EMAIL, // Your email id
-//       pass: process.env.PASS // Your password
-//     }
-//   });
-//
-//   // setup e-mail data with unicode symbols
-//   var mailOptions = {
-//     from: req.body.senderEmail, // sender address
-//     to: process.env.EMAIL, // list of receivers
-//     subject: "From: " + req.body.senderName + ": \n" + req.body.emailSubject, // Subject line
-//     text: req.body.emailMessage, // plaintext body
-//   };
-//
-//   // send mail with defined transport object
-//   transporter.sendMail(mailOptions, function(error, info){
-//     if(error){
-//       console.log('err ', error);
-//       res.json('Server error: ', error);
-//     } else {
-//       console.log('good res ', info.response);
-//       res.json('Message sent: ' + info.response);
-//     }
-//   });
-//
-// });
 
 module.exports = router;
